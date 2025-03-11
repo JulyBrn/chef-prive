0ed2f2e99cae3ceb4193c29f27fb3f25c7cde3a5
July <july.brisson@wmki.fr>
2025-03-11 15:05:57 +0100

    add repository

---
+++ dockerfile
@@ -0,0 +1,17 @@
+FROM php:8.2-fpm
+
+# Installer les dépendances pour PDO MySQL
+RUN apt-get update && apt-get install -y \
+    libpng-dev \
+    libjpeg62-turbo-dev \
+    libfreetype6-dev \
+    libzip-dev \
+    libmariadb-dev \
+    && docker-php-ext-configure zip \
+    && docker-php-ext-install pdo_mysql zip \
+    && apt-get clean
+
+RUN docker-php-ext-install pdo_mysql
+
+# Ajouter composer (si nécessaire)
+RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
---
+++ src/Entity/Prestations.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PrestationsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PrestationsRepository::class)]
+class Prestations
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $titre = null;
+
+    #[ORM\Column(length: 280)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $type = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): static
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+}
