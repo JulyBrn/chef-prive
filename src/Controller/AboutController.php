630baf4aec901f29cb3a5cb26720f15de7d9c028
JulyBrn <julybrn60@gmail.com>
2025-03-11 20:59:26 +0100

    styles and home page

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class AboutController extends AbstractController
+class AboutController extends AbstractController
 {
     #[Route('/about', name: 'about')]
     public function index(): Response
