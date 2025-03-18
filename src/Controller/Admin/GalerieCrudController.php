4dd78a81fd8dbc22633739a055551fa291409514
July <july.brisson@wmki.fr>
2025-03-18 15:49:57 +0100

    mise en place Easy admin

---
+++ src/Controller/Admin/GalerieCrudController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Galerie;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class GalerieCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Galerie::class;
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('description'),
+            ImageField::new('img_url'),
+        ];
+    }
+    
+}
---
+++ src/Controller/Admin/UsersCrudController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Users;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class UsersCrudController extends AbstractCrudController
+{
+    use Traits\ReadOnlyTrait;
+    public static function getEntityFqcn(): string
+    {
+        return Users::class;
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('name'),
+            EmailField::new('email'),
+        ];
+    }
+}
